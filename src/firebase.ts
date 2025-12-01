4fe89e8d6bc3b1ecb932e7c207ef768254252de9
Stella Goodrich <goodrist@mail.gvsu.edu>
2025-11-30 19:46:38 -0500

    firebase auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,25 @@
-import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-export const firebaseConfig = {
-  // COPY this from your Firebase Console
-  apiKey: "your-api-key-goes-here",
-  authDomain: "your-project-name-here.firebaseapp.com",
-  databaseURL: "https://your-project-name-here.firebaseio.com",
-  projectId: "your-project-name-here",
-  storageBucket: "your-project-name.appspot.com",
-  messagingSenderId: "xxxxxxxx",
+// Your config
+const firebaseConfig = {
+  apiKey: "AIzaSyC3W4V060V6LIQI5qPXUPfPC4eIGSKy3vc",
+  authDomain: "cis371-9e5b2.firebaseapp.com",
+  projectId: "cis371-9e5b2",
+  storageBucket: "cis371-9e5b2.firebasestorage.app",
+  messagingSenderId: "251984419526",
+  appId: "1:251984419526:web:1d8867c1d835e31b3055c7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firestore
 const db = getFirestore(app);
 
+// Auth
+export const auth = getAuth(app);
+
+// DEFAULT EXPORT REQUIRED BY YOUR STORE
 export default db;
